5919083fc206b08c657d796e18671b69b1afd366
Eric Hansen <echansen@nabancard.com>
2017-03-18 21:56:00 -0400

    Renamed helm-sql-mode to helm-sql-connect AND made some nice refactoring changes

--- helm-sql-connect.el
+++ helm-sql-connect.el
@@ -1,44 +1,43 @@
-;;; helm-sql-mode.el --- Choose a database to connect to via Helm.
+;;; helm-sql-connect.el --- Choose a database to connect to via Helm.
 ;; Copyright 2017 Eric Hansen
 ;;
 ;; Author: Eric Hansen <hansen.c.eric@gmail.com>
 ;; Maintainer: Eric Hansen <hansen.c.eric@gmail.com>
 ;; Keywords: tools convenience comm
-;; URL: https://github.com/eric-hansen/helm-sql-mode
+;; URL: https://github.com/eric-hansen/helm-sql-connect
 ;; Created: 1st March 2017
 ;; Version: 0.0.1
 ;; Package-Requires: ((helm "0.0.0"))
 
 ;;; Commentary:
 ;;
-;; Only Postgres is supported right now, but fixing that is definitely on the TODO.
+;; Pretty cool, huh?!
 
 ;;; Code:
 
 (require 'sql)
 (require 'helm)
 
-(defun helm-sql-connect ()
+(defun helm-sql-connect-to ()
   "Populate helm buffer with connection string names from a populated sql-connection-alist."
   (with-helm-current-buffer
-    (mapcar (lambda (element)
-          (let ((key (car element))
-                (value (cdr element)))
-            (message "%s" key))) sql-connection-alist)))
+    (let (connection-names '())
+      (setq connection-names (mapcar 'car sql-connection-alist))
+      connection-names)))
 
 (defvar helm-sql-connection-pool
-  '((name . "PGSQL Connections")
-    (candidates . helm-sql-connect)
+  '((name . "SQL Connections")
+    (candidates . helm-sql-connect-to)
     (action . (lambda (connection)
                 (sql-connect connection)))))
 
 ;;;###autoload
-(defun helm-sql-connect-to ()
+(defun helm-sql-connect ()
   "Helm directive to call when wanting to list SQL connections to connect to."
   (interactive)
   (helm :sources '(helm-sql-connection-pool)
-        :buffer "*sql-connection*"))
+        :buffer "*helm-sql-connect*"))
 
-(provide 'helm-sql-mode)
+(provide 'helm-sql-connect)
 
-;;; helm-sql-mode.el ends here
+;;; helm-sql-connect.el ends here
